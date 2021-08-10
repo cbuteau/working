ae2b0357ba06750ba3c20398cc1ef398ad410582
Christopher Buteau <cbuteau@gmail.com>
2021-08-09 20:30:29 -0400

    closer

--- src/workqueue.js
+++ src/workqueue.js
@@ -2,6 +2,8 @@
 import { PromiseParts } from "./promiseparts"
 import { RafRepeater } from "./rafrepeater"
 
+const MAX_EMPTY_PASSES = 3;
+
 class WorkQueue {
   constructor() {
     this._raf = new RafRepeater(this._think.bind(this));
@@ -14,11 +16,34 @@ class WorkQueue {
       task: task
     }
     this._queue.push(obj);
-    this._raf.start();
+    this.start();
     return obj.parts.promise;
   }
 
-  _think() {
+  start() {
+    this.emptyPasses = 0;
+    this._raf.start();
+  }
 
+  _think() {
+    let counter;
+
+    while (this._queue.length) {
+      let current = this._queue.pop();
+      current.task.execute.call(current.task, current.parts);
+      counter++;
+    }
+
+    if (counter === 0) {
+      this.emptyPasses++;
+      if (this.emptyPasses >= MAX_EMPTY_PASSES) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
+
+const workqueue = new WorkQueue();
+export { workqueue };
